b15ae6392ae527293a6e9a331eed07600f36f446
Craig Harvey <craig.harvey@elabor8.com.au>
2025-08-06 10:45:00 +1000

    Return bus number and estimate

--- ExtractBusTimes.js
+++ ExtractBusTimes.js
@@ -1,12 +1,19 @@
 const axios = require('axios');
 
-async function fetchData() {
+var stopId = '001429';  // Stop ID for the bus stop
+
+async function fetchBusInfo(stopId) {
   try {
-    const response = await axios.get('https://jp.translink.com.au/api/stop/timetable/001924');
-    console.log(response.data);
+    const response = await axios.get('https://jp.translink.com.au/api/stop/timetable/' + stopId);
+    var nextBus = {
+        "busNumber": response.data.departures[0].headsign,
+        "due": response.data.departures[0].departureDescription
+    };
+    console.log(nextBus);
+    return nextBus;
   } catch (error) {
     console.error(error);
   }
 }
 
-fetchData();
+fetchBusInfo(stopId);
--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-# Bus_Times_Google_Action
+# When's My Next Bus - Alexa Skill
 
-This is a simple app that connects into the Brisbane translink API to get the next bus estimated time (real-time).
+This is a simple app that connects into the Brisbane translink API to get the next bus estimated time (real-time). Only works on a pre-configured specific bus stop
 
-
-## URL
+## Translink URL
 
 The URL for the bus stop near my place is https://jp.translink.com.au/api/stop/timetable/001924
 
